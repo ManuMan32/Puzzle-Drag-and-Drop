8cc782ce5b73955e431d79f6c2e2aa0ec522a0fe
Manu <141773224+ManuMan32@users.noreply.github.com>
2024-08-11 09:13:35 -0300

    Code review 11-08-2024
    Add comments, better indentation and improved functions structure

--- script.js
+++ script.js
@@ -8,30 +8,34 @@ let pieces;
 
 // ---- Creation of the puzzle ----
 
-function sortPieces(number) {
+function sortPieces() {
+	// Randomize the position of the pieces inside a "deckPieces" array
+	// This array is made to check if a piece is already created
+	// deckPieces[n] == 0 means that the piece hasn't been created
+	// deckPieces[n] == 1 means that the piece has been created
 	const deckPieces = [];
 	let j = 1;
-	while (j < number+1) {
+	while (j < piecesNumber + 1) {
 		deckPieces[j] = 0;
 		j++;
 	}
 
 	j = 1;
-	while (j < number+1) {
-		createPiece(Math.floor(Math.random()*17+1));
+	while (j < piecesNumber + 1) {
+		createPiece(Math.floor(Math.random() * 17 + 1));
 		j++;
 	}
 
 	function createPiece(p) {
-		if (p > number) createPiece(1);
-		else if (deckPieces[p] == 1) createPiece(p+1);
+		if (p > piecesNumber) createPiece(1);
+		else if (deckPieces[p] == 1) createPiece(p + 1);
 		else {
 			const piece = document.createElement("div");
 			piece.classList.add("grid-piece");
 			piece.id = getPieceId();
 			const img = document.createElement("img");
 			img.alt = p;
-			img.src = "Pieces/"+p+".png";
+			img.src = "Pieces/" + p + ".png";
 			piece.appendChild(img);
 			piecesBox.appendChild(piece);
 			deckPieces[p] = 1;
@@ -40,35 +44,37 @@ function sortPieces(number) {
 }
 
 function getPieceId() {
+	// Creates an unique ID that doesn't exist in the DOM
 	while(true) {
-		const id = Math.floor(Math.random()*9999)+1;
-		if (document.getElementById("Piece"+id) === null) {
-			return "Piece"+id;
+		const id = Math.floor(Math.random() * 9999) + 1;
+		if (document.getElementById("Piece" + id) === null) {
+			return "Piece" + id;
 		}
 	}
 }
 
 function removePieces() {
-	pieces.forEach(i=> i.style.animation = "1s ease forwards disapear" );
-	setTimeout(()=>{
-		pieces.forEach(i=>i.remove());
-	},1300);
+	// Removes all the pieces with an animation
+	pieces.forEach(i => i.style.animation = "1s ease forwards disapear");
+	setTimeout(() => {
+		pieces.forEach(i => i.remove());
+	}, 1300);
 }
 
-sortPieces(piecesNumber);
-pieces = document.querySelectorAll(".grid-piece");
+sortPieces(); // Sorts the pieces and starts the game
+pieces = document.querySelectorAll(".grid-piece"); // Saves all the pieces in a nodeList after sorting them
 
 // --- Drag and drop actions ----
 
-function dragstartFunction(e) {
+function dragStart(e) {
 	this.style.opacity = "0.5";
 	if (this.children.length == 1) {
 		e.dataTransfer.clearData();
-		e.dataTransfer.setData("text",this.id);
+		e.dataTransfer.setData("text", this.id);
 	}
 }
-function dragoverFunction(e) { e.preventDefault(); };
-function dropFunction(e) {
+function dragOver(e) { e.preventDefault() }
+function drop(e) {
 	e.preventDefault();
 	const data = e.dataTransfer.getData("text");
 	if (data.length != 2) {
@@ -79,35 +85,38 @@ function dropFunction(e) {
 			element.appendChild(this.children[0]);
 			element.style.opacity = "1";
 		}
-		comprobateComplete();
+		comprobateComplete(); // Checks if the puzzle if completed
 	}
 }
 
-function callEvents() {
-	pieces.forEach(i=>{
-		i.addEventListener("dragstart",dragstartFunction);
-		i.addEventListener("dragend",function() { this.style.opacity = "1"; });
-		i.addEventListener("dragover",dragoverFunction);
-		i.addEventListener("drop",dropFunction);
+function addEvents() {
+	// Adds the event listeners to all the pieces
+	pieces.forEach(i => {
+		i.addEventListener("dragstart", dragStart);
+		i.addEventListener("dragend", function() { this.style.opacity = "1" });
+		i.addEventListener("dragover", dragOver);
+		i.addEventListener("drop", drop);
 	});
 }
 
 function removeEvents() {
-	pieces.forEach(i=>{
-		i.removeEventListener("dragstart",dragstartFunction);
-		i.removeEventListener("dragover",dragoverFunction);
-		i.removeEventListener("drop",dropFunction);
+	// Removes the event listeners to all the pieces
+	pieces.forEach(i => {
+		i.removeEventListener("dragstart", dragStart);
+		i.removeEventListener("dragover", dragOver);
+		i.removeEventListener("drop", drop);
 	});
 }
 
-callEvents();
+addEvents();
 
 // ---- Various functions ----
 
 function comprobateComplete() {
+	// Checks if every piece is in the right place and finishes the game
 	const piecesList = document.querySelectorAll(".grid-piece");
 	for (let i in piecesList) {
-		if (piecesList[i].children[0].alt == parseInt(i)+1) {
+		if (piecesList[i].children[0].alt == parseInt(i) + 1) {
 			if (i == 17) {
 				makeAnimationPieces();
 				break;
@@ -117,21 +126,27 @@ function comprobateComplete() {
 }
 
 function makeAnimationPieces() {
+	// Creates the final animation for the pieces
 	const piecesList = document.querySelectorAll(".grid-piece");
 	for (let i in piecesList) {
 		if (typeof piecesList[i] == "object") {
 			removeEvents();
-			setTimeout(()=>{
+			setTimeout(() => {
 				piecesList[i].style.animation = "2s ease-in-out forwards upPiece";
-			},100*i);
+			}, 100 * i);
 		}
 	}
 	setTimeout(()=>{
-		removePieces();
-		setTimeout(()=>{
-			sortPieces(piecesNumber);
-			pieces = document.querySelectorAll(".grid-piece");
-			callEvents();
-		},1300);
-	},3100);
+		resetGame();
+	}, 3100);
+}
+
+function resetGame() {
+	// Resets all the pieces using an animation
+	removePieces();
+	setTimeout(() => {
+		sortPieces(piecesNumber);
+		pieces = document.querySelectorAll(".grid-piece");
+		addEvents();
+	}, 1300);
 }
